109159435583a17e07c06a2c253dc8596a6f0e94
LupusEternus <wilk.tomek@gmail.com>
2024-08-07 16:42:17 +0200

    next problems

--- src/DP/PascalsTriangle_118.java
+++ src/DP/PascalsTriangle_118.java
@@ -2,21 +2,27 @@ package DP;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PascalsTriangle_118 {
     public static List<List<Integer>> generate(int numRows) {
-
-
-
-
-        return null;
-    }
-
-    public static void main(String[] args) {
-
-
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) {
+            return result;
+        }
+        List<Integer> firstRow = List.of(1);
+        result.add(firstRow);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = result.get(i - 1);
+            List<Integer> currentRow = new ArrayList<>();
+            currentRow.add(1);
+            for (int j = 1; j < i; j++) {
+                currentRow.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            currentRow.add(1);
+            result.add(currentRow);
+        }
+        return result;
     }
 }
 
