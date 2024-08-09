009fca4c0409a60d39efb27479b79854f65c82bd
LupusEternus <wilk.tomek@gmail.com>
2024-08-09 11:13:23 +0200

    next problem

---
+++ src/DP/MinCostClimbingStairs_746.java
@@ -0,0 +1,15 @@
+package DP;
+
+
+public class MinCostClimbingStairs_746 {
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        dp[dp.length - 1] = cost[cost.length - 1];
+        dp[dp.length - 2] = cost[cost.length - 2];
+        for (int i = cost.length - 3; i >= 0; i--) {
+            dp[i] = cost[i] + Math.min(dp[i + 1], dp[i + 2]);
+        }
+        return Math.min(dp[0], dp[1]);
+    }
+}
