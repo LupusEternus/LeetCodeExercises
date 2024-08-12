1a2d46fbcb1d39d1e76bf579514f14494858c08b
LupusEternus <wilk.tomek@gmail.com>
2024-08-12 16:01:47 +0200

    next problem

---
+++ src/DP/MaximumRepeatingSubstring_1668.java
@@ -0,0 +1,15 @@
+package DP;
+
+public class MaximumRepeatingSubstring_1668 {
+
+    public int maxRepeating(String sequence, String word) {
+        if(word.length() > sequence.length()) return 0;
+        int count = 0;
+        StringBuilder sb = new StringBuilder(word);
+        while(sequence.contains(sb)){
+            count++;
+            sb.append(word);
+        }
+        return count;
+    }
+}
