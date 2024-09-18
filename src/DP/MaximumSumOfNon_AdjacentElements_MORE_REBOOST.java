c4110d7de4b64a955d4d8d8e8ba59d9e7e44d5d6
LupusEternus <wilk.tomek@gmail.com>
2024-09-18 21:19:33 +0200

    next problems

--- src/DP/MaximumSumOfNon_AdjacentElements_MORE_REBOOST.java
+++ src/DP/MaximumSumOfNon_AdjacentElements_MORE_REBOOST.java
@@ -1,25 +1,32 @@
 package DP;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MaximumSumOfNon_AdjacentElements_MORE_REBOOST {
 
     public static int maximumSumOfNonAdjElements(List<Integer> list) {
-
-        return helper(list, list.size()-1 );
+        int[] seen = new int[list.size()+1];
+        Arrays.fill(seen,-1);
+        return helper(list, list.size()-1,seen );
     }
 
-    private static int helper(List<Integer> list, int index) {
+    private static int helper(List<Integer> list, int index,int[] seen) {
+
         if(index < 0){
             return 0;
+        }
+        if(seen[index] != -1){
+            return seen[index];
         }
         if (index == 0) {
            return list.get(0);
         }
-        int pick = list.get(index) + helper(list, index - 2);;
-        int notpick = helper(list, index - 1);;
+        int pick = list.get(index) + helper(list, index - 2,seen);;
+        int notpick = helper(list, index - 1,seen);;
 
+        seen[index] = Math.max(pick, notpick);
         return Math.max(pick, notpick);
     }
 
---
+++ src/DP/NinjaTraining.java
@@ -0,0 +1,13 @@
+package DP;
+
+import java.util.List;
+
+public class NinjaTraining {
+
+    public static int ninjaTraining(int days, List<List<Integer>> points){
+
+
+        return 0;
+    }
+
+}
