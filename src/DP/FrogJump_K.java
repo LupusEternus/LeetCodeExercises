5c36cd090c41694729151bb21864c616c82b667b
LupusEternus <wilk.tomek@gmail.com>
2024-09-20 20:59:50 +0200

    problem statement to Frog K jump

--- src/DP/FrogJump_K.java
+++ src/DP/FrogJump_K.java
@@ -2,6 +2,12 @@ package DP;
 
 import java.util.*;
 
+/*
+This is a follow-up question to “Frog Jump” discussed in the previous article. In the previous question, the frog was allowed
+to jump either one or two steps at a time. In this question, the frog is allowed to jump up to ‘K’ steps at a time.
+If K=4, the frog can jump 1,2,3, or 4 steps at every index.
+*/
+
 public class FrogJump_K {
 
     public static int frogJump(int target, ArrayList<Integer> heights, int k){
