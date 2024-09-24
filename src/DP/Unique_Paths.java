4bb88d484d9538db70d9d1f37b59d7d953359c53
Tomek Wilk <tomasz.a.wilk@capgemini.com>
2024-09-24 16:37:05 +0200

    Unique_Paths refactor

--- src/DP/Unique_Paths.java
+++ src/DP/Unique_Paths.java
@@ -2,17 +2,17 @@ package DP;
 
 public class Unique_Paths {
 
-    public static int helper(int[][] matrix, int x_pos, int y_pos, int m, int n) {
+    public static int helper(int x_pos, int y_pos, int m, int n) {
 
         if (x_pos == m - 1 && y_pos == n - 1) {
             return 1;
         }
         int paths = 0;
-        if (x_pos < matrix.length - 1) {
-            paths += helper(matrix, x_pos + 1, y_pos, m, n);
+        if (x_pos < m) {
+            paths += helper(x_pos + 1, y_pos, m, n);
         }
-        if (y_pos < matrix[x_pos].length - 1) {
-            paths += helper(matrix, x_pos, y_pos + 1, m, n);
+        if (y_pos < n) {
+            paths += helper( x_pos, y_pos + 1, m, n);
         }
         return paths;
 
@@ -20,8 +20,7 @@ public class Unique_Paths {
 
     public static int uniquePaths(int m, int n) {
 
-        int[][] matrix = new int[m][n];
-        return helper(matrix, 0, 0, m, n);
+        return helper(0, 0, m, n);
     }
 
     public static void main(String[] args) {
